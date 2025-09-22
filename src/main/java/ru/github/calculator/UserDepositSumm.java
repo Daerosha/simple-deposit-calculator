7066275ddecb4491c07634821d569acd3480c3f1
Daerosha <Daerosha@gmail.com>
2025-09-22 20:19:14 +0300

    Update README.md
    rename for PascalCase
    
    Fix DecimalFormat, изменил перевод умножения в long
    
    Разделил расчеты и вывод, добавил для вывода разделение для тысячных зарядов

--- src/main/java/ru/github/calculator/UserDepositSumm.java
+++ src/main/java/ru/github/calculator/UserDepositSumm.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 class UserDepositSumm {
     public int GetDepositSumm() throws IOException {
-        System.out.print("Введите сумму депозита (от 1 до 100 000 000): ");
-        return GetUserInput.UserInput(1, 100000000);
+        System.out.print("Введите сумму депозита (от 1 до 30 000 000): ");
+        return GetUserInput.UserInput(1, 30000000);
 
     }
 }
