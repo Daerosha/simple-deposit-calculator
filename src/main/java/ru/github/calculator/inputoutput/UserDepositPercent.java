c8461f4e59d5d989d74c8cd943a66270b9357429
Daerosha <Daerosha@gmail.com>
2025-10-19 02:06:32 +0300

    Заменил язык приложения на англиский, поправил комменты по gradle
    Заменил язык приложения на англиский, поправил комменты по gradle
    
    Заменил язык приложения на англиский, поправил комменты по gradle
    
    Добавил gradle
    
    # The commit message #14 will be skipped:
    
    # Добавил gradle
    
    Добавил gradle

--- src/main/java/ru/github/calculator/inputoutput/UserDepositPercent.java
+++ src/main/java/ru/github/calculator/inputoutput/UserDepositPercent.java
@@ -13,17 +13,17 @@ public class UserDepositPercent implements UserDepositValue {
 
     @Override
     public int getDepositValue()  {
-        userDepositPercent.post("Введите процент депозита (от " + MIN_PERCENT + " до " + MAX_PERCENT + "): ");
+        userDepositPercent.post("%nEnter the deposit percentage (between " + MIN_PERCENT + " and " + MAX_PERCENT + "):");
         while (true) {
             try {
                 int percent = Integer.parseInt(userDepositPercent.getDataForCalculation());
                 if (percent < MIN_PERCENT || percent > MAX_PERCENT) {
-                    userDepositPercent.post("Вы ввели неверное число, оно должно быть от " + MIN_PERCENT + " до " + MAX_PERCENT + ", повторите попытку:");
+                    userDepositPercent.post("%nYou entered an incorrect number, it must be between " + MIN_PERCENT + " and " + MAX_PERCENT + ", please try again:");
                 } else {
                     return percent;
                 }
             } catch (NumberFormatException | IOException e) {
-                userDepositPercent.post("Ошибка: вы ввели слишком большое, либо нецелое число при вводе процента! Повторный ввод:");
+                userDepositPercent.post("%nError: You entered a number that is too large or not an integer when entering the percentage! Re-enter:");
             }
 
         }
