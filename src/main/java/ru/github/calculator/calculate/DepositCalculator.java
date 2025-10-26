e3d195bdfa580561dc25fb4fa542d96ed1de6f9b
Daerosha <Daerosha@gmail.com>
2025-10-27 02:04:55 +0300

    Добавил unitTest на класс DepositCalculator и DepositCalculatorResultTest + поменял умножение в summStonks

--- src/main/java/ru/github/calculator/calculate/DepositCalculator.java
+++ src/main/java/ru/github/calculator/calculate/DepositCalculator.java
@@ -8,9 +8,10 @@ public class DepositCalculator {
     private static final long KOPEYKI = 100L;
 
     public DepositCalculationResult startCalculate(DepositValueForCalculation valueForCalculating) {
-        long summStonks =  (long) ((long) valueForCalculating.sum() * valueForCalculating.term() * valueForCalculating.percent() / MONTH_IN_YEAR);
-        long summTotal = summStonks + valueForCalculating.sum() * KOPEYKI;
-        return new DepositCalculationResult(summTotal, summStonks);
+            long summStonks = (long) (Math.multiplyExact(Math.multiplyExact((long)valueForCalculating.sum(), valueForCalculating.term()), (valueForCalculating.percent()))/ MONTH_IN_YEAR);
+            long summTotal = summStonks + valueForCalculating.sum() * KOPEYKI;
+            return new DepositCalculationResult(summTotal, summStonks);
+
     }
 }
 
